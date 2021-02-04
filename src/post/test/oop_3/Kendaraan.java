2dce24069acf728b9ed1f16137fdeb9133cb456f
MohammadBagusAndinPrakoso <m._bagus_29rpl@student.smktelkom-mlg.sch.id>
2021-02-04 22:28:22 +0700

    Post-Test OOP Soal Praktikum No. 3

---
+++ src/post/test/oop_3/Kendaraan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package post.test.oop_3;
+
+/**
+ *
+ * @author MOKLET1
+ */
+public class Kendaraan {
+    
+}
